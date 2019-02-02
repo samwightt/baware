a3b846c6860c6a00fa6da248e08b9c4dbe837a74
Sam Wight <samuelwight@gmail.com>
2019-02-02 17:52:59 -0600

    Updated.
    Signed-off-by: Sam Wight <samuelwight@gmail.com>

--- client/collections.js
+++
@@ -1,4 +0,0 @@
-import { Mongo } from 'meteor/mongo';
-
-export const People = new Mongo.Collection('people');
-export const Conversations = new Mongo.Collection('conversations');
--- server/main.js
+++ server/main.js
@@ -1,14 +1,5 @@
 import { Meteor } from 'meteor/meteor';
-import { Mongo } from 'meteor/mongo';
 
 Meteor.startup(() => {
-    // code to run on server at startup
-});
-
-People = new Mongo.Collection('people');
-Conversations = new Mongo.Collection('conversations');
-
-People.insert({
-    number: "+12056502938",
-    location: "locationhere",
+  // code to run on server at startup
 });
