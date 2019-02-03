f7305d6c028ad6d387645ac5667ce6eb07f20c3e
Sam Wight <samuelwight@gmail.com>
2019-02-02 19:06:27 -0600

    Num sent stat added.
    Signed-off-by: Sam Wight <samuelwight@gmail.com>

--- client/stats/sent.js
+++ client/stats/sent.js
@@ -1,1 +1,9 @@
 import './sent.html';
+import { Template } from 'meteor/templating';
+import { Conversations } from '../collections';
+
+Template.sent.helpers({
+  numSent: function() {
+    return Conversations.find({}).count();
+  }
+})
