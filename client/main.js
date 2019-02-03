b9e2306d02bcb427d0af14d806d45aa084ab8504
Sam Wight <samuelwight@gmail.com>
2019-02-02 18:24:11 -0600

    Added skeleton views.
    Signed-off-by: Sam Wight <samuelwight@gmail.com>

--- client/main.js
+++ client/main.js
@@ -1,3 +1,7 @@
 import { Template } from 'meteor/templating';
 
 import './main.html';
+import './stats/received.js';
+import './stats/sent.js';
+import './blobs/showBlobs.js';
+import './blobs/blobStats.js';
---
+++ client/stats/received.js
@@ -0,0 +1,1 @@
+import './received.html';
